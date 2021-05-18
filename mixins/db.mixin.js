bcd36657551ee67fbf3c731d035af9f7aa543fbe
Jaime Dordio <jaimedordio@gmail.com>
2021-05-19 01:18:32 +0200

    Comment cleanup & deploy test

--- mixins/db.mixin.js
+++ mixins/db.mixin.js
@@ -6,20 +6,6 @@ const mkdir = require("mkdirp").sync;
 const DbService = require("moleculer-db");
 
 module.exports = function (collection) {
-  // if (process.env.MONGO_URI) {
-  //   // Mongo adapter
-  //   const MongoAdapter = require("moleculer-db-adapter-mongo");
-
-  //   return {
-  //     mixins: [DbService],
-  //     adapter: new MongoAdapter(process.env.MONGO_URI, {
-  //       useNewUrlParser: true,
-  //       useUnifiedTopology: true,
-  //     }),
-  //     collection,
-  //   };
-  // }
-
   // Make data folder directory
   mkdir(path.resolve("./data"));
 
